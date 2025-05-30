4fcdb9d800a3e72e9daac68d2654b6357683c360
mikis1111 <michelesaladino11@gmail.com>
2025-05-30 13:49:05 +0200

    creato i contenuti

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contents
   devise_for :users
   root to: "pages#home"
   get "chi_sono", to: "pages#chi_sono"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_30_062153) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_30_112021) do
+  create_table "contents", force: :cascade do |t|
+    t.string "titolo"
+    t.text "descrizione"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
